f9af418673bfa19209ab9dcc7ffbd86ad4cbb4c8
Condoriano <15719920+jd4883@users.noreply.github.com>
2021-12-03 19:09:00 -0800

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,2 +1,5 @@
 data "lastpass_secret" "credentials" { id = data.external.credentials.result.id }
-data "external" "credentials" { program = ["${path.module}/lastpass_lookup.sh", var.secret] }
+data "external" "credentials" {
+  provisioner "local-exec" { command = "chmod +x ${path.module}/lastpass_lookup.sh" }
+  program = ["${path.module}/lastpass_lookup.sh", var.secret]
+}
