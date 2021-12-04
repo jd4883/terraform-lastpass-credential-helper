ba572b23b19792b168f80f4392ed20a92817558a
Condoriano <15719920+jd4883@users.noreply.github.com>
2021-12-03 19:22:17 -0800

    Update data.tf
    fixes data lookups to force a chmod on the script

--- data.tf
+++ data.tf
@@ -1,5 +1,9 @@
 data "lastpass_secret" "credentials" { id = data.external.credentials.result.id }
 data "external" "credentials" {
-  provisioner "local-exec" { command = "chmod +x ${path.module}/lastpass_lookup.sh" }
   program = ["${path.module}/lastpass_lookup.sh", var.secret]
+  depends_on = [null_resource.script]
+}
+
+resource "null_resource" "script" {
+  provisioner "local-exec" { command = "chmod +x ${path.module}/lastpass_lookup.sh" }
 }
